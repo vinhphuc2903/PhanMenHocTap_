59a92bc6d835ff2ebd601b7087f4abf746ae8b7b
Vĩnh Phúc Nguyễn Phước <npvphucddung92@gmail.com>
2021-10-16 11:56:33 +0700

    Add dang nhap-dang ki

---
+++ DK_DN/DangKy/vadidator.js
@@ -0,0 +1,18 @@
+//Hàm validator: constructer function 
+function Validator(options)
+{
+    var formElement = document.querySelector(options.form);
+    
+    if(formElement)
+    {
+       
+    }
+}
+Validator.isRequired = function(selector)
+{
+    return selector;
+}
+Validator.isEmail = function()
+{
+    return 2;
+}
